54d12b28862acc62d26cb199a18406325080a516
Ali Jabir <syedalijabir@gmail.com>
2025-07-16 17:26:05 -0700

    chore: move helpers to utils dir

---
+++ backend/utils/auth.py
@@ -0,0 +1,18 @@
+import jwt
+from flask import request
+import os
+
+SECRET_KEY = os.getenv('JWT_SECRET')
+
+def get_user_id_from_token():
+    auth_header = request.headers.get("Authorization")
+    if not auth_header or not auth_header.startswith("Bearer "):
+        return None
+    token = auth_header.split(" ")[1]
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=["HS256"])
+        return payload.get("user_id")
+    except jwt.ExpiredSignatureError:
+        return None
+    except jwt.InvalidTokenError:
+        return None
